0aa2802222657e6b8b4bd30ac977712851840354
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-10 15:57:38 +0530

    Added: Employee is Present or Absent

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,21 @@
-class EmpWage
+import java.util.Random;
+
+public class EmpWage
 {
 	public static void main(String agrs[])
-	{
+	{	
 		System.out.println("Welcome to Employee Wage Compuation");
+
+		Random rand=new Random();
+		int i=rand.nextInt(2);
+		int isPresent=1;
+		if(i==1)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 	}
 }
