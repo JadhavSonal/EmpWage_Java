14c9c5205351e2c18d2092987dab554341c637b8
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-11 22:08:43 +0530

    Added: Refactor the code of Emp

--- EmpWage.java
+++ EmpWage.java
@@ -1,65 +1,43 @@
-import java.io.*;
 import java.util.Random;
-import java.util.Scanner;
-public class EmpWage
-{
-
-	public void empSal( )
-	{
-		System.out.println("Welcome to Employee Wage Problem");
-        	Random rand=new Random();
-        	int i=rand.nextInt(2);
-        	int WagePerHour=20;
-        	int HourPerDay=8,empHour=0,day=1;
-        	String empName=" ";
-        	int PartTimeHoursPerDay=4;
-        	int salary=0 , FullTime=1 , PartTime=2;
-		int totalSal=0;
-        	if (i == 1)
-        	{
-                	System.out.println("Employee Present");
-        	}
-        	else
-        	{
-                	System.out.println("Employee Absent");
-        	}
-        	System.out.println("****************************************");
-       		System.out.println("Calculating Daily wage Of Employee");
-        	int WagePerDay=(WagePerHour * HourPerDay);
-        	System.out.println("Daily Employee wage:"+WagePerDay);
-
-        	System.out.println("****************************************");
-        	System.out.println("Calculate wages for month");
-        	System.out.println("Employee wage");
-
-        	System.out.println("Enter number of working days");
-		while(day<=20 && empHour<=100)
-		{
-			int empCheck=rand.nextInt(2);
-			switch(empCheck){
-			case 0 :
-				empName="FullTime";
-				empHour=empHour+8;
-				break;
-			case 1 :
-				empName="PartTime";
-				empHour=empHour+4;
-				break;
+class empSal{
+	int Wage_per_hour=20,Day_Hour=0,Total_Wage=1;
+	int count=1,hours=0,Total_Month_Wage=0;
+	Random rand = new Random();
+	void check(){
+		while(count<21&&hours<100){
+			int ch = rand.nextInt(10) % 3;
+			switch (ch){
+				case 0:
+					System.out.println("Present Full time employee");
+					Day_Hour=8;
+					break;
+				case 1:
+					
+					System.out.println("Present Part time employee");
+					Day_Hour=4;
+					break;
+				default:
+					
+					System.out.println("Invalid choice");
+					Day_Hour=0;
+					break;
+			}
+			hours=hours+Day_Hour;
+			Total_Wage=Wage_per_hour*Day_Hour;
+			Total_Month_Wage=Total_Month_Wage+Total_Wage;
+			System.out.println("Total Wage of "+count+" Day : "+Total_Wage);
+			count=count+1;
 		}
-		salary=(empHour*WagePerHour);
-		System.out.println("Salary of " + empName + " on the " +day+ " is " +salary);
-		totalSal=(totalSal+salary);
-		day++;
+	
+		System.out.println("Total Month Wage : "+Total_Month_Wage);
+		System.out.println("Total Month Work Hours : "+hours);
+		
 	}
-	System.out.println("Total Employee Wage="+totalSal);
-
 }
-	public static void main(String[] args)
-	{
-		EmpWage ew=new EmpWage();
-		ew.empSal();
+public class EmpWage{
+	public static void main(String args[]){
+		System.out.println("Welcome to Employee Wage Computation Program");
+		empSal e1=new empSal();
+		e1.check();
 	}
-
-
 }
-
