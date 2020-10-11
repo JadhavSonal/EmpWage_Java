f9968a88d8f528f1b6893ac186c10e686be7cf18
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-11 13:39:28 +0530

    Added:Used Switch Case to find total Salary

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-
+import java.util.*;
 public class EmpWage
 {
 	public static void main(String agrs[])
@@ -9,9 +9,9 @@ public class EmpWage
 		System.out.println("Welcome to Employee Wage Compuation");
 		Random rand=new Random();
 		int i=rand.nextInt(2);
-		int isPresent=1;
-		int WagePerHour=20, HourPerDay=8, PartTimeHour=4;
-
+		int WagePerHour=20, HourPerDay=8, PartTimeHour=4,empHour=0;
+		String empName=" ";
+		int salary=0, totalSal=0,FullTime=1,PartTime=2;
 		if(i==1)
 		{
 			System.out.println("Employee is present");
@@ -28,7 +28,26 @@ public class EmpWage
                 int WagePerDay1 = (WagePerHour * PartTimeHour);
                 System.out.println("Part time Employee Wage:"+WagePerDay1);
 
-
-		
+		System.out.println("Enter number of working days");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		for(int day=1;day<=num;day++)
+		{
+		int empCheck=rand.nextInt(2);
+		switch(empCheck){
+		case 0 :
+			empName="FullTime";
+			empHour=8;
+			break;
+		case 1 :
+			empName="PartTime";
+			empHour=4;
+			break;
+		}
+		salary=(empHour*WagePerHour);
+		System.out.println("Salary of " + empName + " on the " +day+ " is " +salary);
+		totalSal=(totalSal+salary);
+	}
+	System.out.println("Total Salary="+totalSal);
 	}
 }
