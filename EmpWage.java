2e4bea3322e392d647619c30291177bb3dcc0508
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-11 21:37:35 +0530

    Added: Refactor the code to write a class method to compute EmpWage

--- EmpWage.java
+++ EmpWage.java
@@ -1,54 +1,65 @@
+import java.io.*;
 import java.util.Random;
-import java.util.*;
+import java.util.Scanner;
 public class EmpWage
 {
-	public static void main(String agrs[])
+
+	public void empSal( )
 	{
+		System.out.println("Welcome to Employee Wage Problem");
+        	Random rand=new Random();
+        	int i=rand.nextInt(2);
+        	int WagePerHour=20;
+        	int HourPerDay=8,empHour=0,day=1;
+        	String empName=" ";
+        	int PartTimeHoursPerDay=4;
+        	int salary=0 , FullTime=1 , PartTime=2;
+		int totalSal=0;
+        	if (i == 1)
+        	{
+                	System.out.println("Employee Present");
+        	}
+        	else
+        	{
+                	System.out.println("Employee Absent");
+        	}
+        	System.out.println("****************************************");
+       		System.out.println("Calculating Daily wage Of Employee");
+        	int WagePerDay=(WagePerHour * HourPerDay);
+        	System.out.println("Daily Employee wage:"+WagePerDay);
 
+        	System.out.println("****************************************");
+        	System.out.println("Calculate wages for month");
+        	System.out.println("Employee wage");
 
-		System.out.println("Welcome to Employee Wage Compuation");
-		Random rand=new Random();
-		int i=rand.nextInt(2);
-		int WagePerHour=20, HourPerDay=8, PartTimeHour=4,empHour=0;
-		String empName=" ";
-		int salary=0, totalSal=0,FullTime=1,PartTime=2,day=1;
-		if(i==1)
-		{
-			System.out.println("Employee is present");
-		}
-		else
-		{
-			System.out.println("Employee is Absent");
-		}
-		System.out.println("Calculate Daily Employee Wage");
-		int WagePerDay = (WagePerHour * HourPerDay);
-		System.out.println("Daily Employee Wage:"+WagePerDay);
-		
-		System.out.println("Calculate Part time Employee Wage");
-                int WagePerDay1 = (WagePerHour * PartTimeHour);
-                System.out.println("Part time Employee Wage:"+WagePerDay1);
-
-		System.out.println("Enter number of working days");
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
+        	System.out.println("Enter number of working days");
 		while(day<=20 && empHour<=100)
 		{
-		int empCheck=rand.nextInt(2);
-		switch(empCheck){
-		case 0 :
-			empName="FullTime";
-			empHour=8;
-			break;
-		case 1 :
-			empName="PartTime";
-			empHour=4;
-			break;
+			int empCheck=rand.nextInt(2);
+			switch(empCheck){
+			case 0 :
+				empName="FullTime";
+				empHour=empHour+8;
+				break;
+			case 1 :
+				empName="PartTime";
+				empHour=empHour+4;
+				break;
 		}
 		salary=(empHour*WagePerHour);
 		System.out.println("Salary of " + empName + " on the " +day+ " is " +salary);
 		totalSal=(totalSal+salary);
 		day++;
 	}
-	System.out.println("Total Salary="+totalSal);
-	}
+	System.out.println("Total Employee Wage="+totalSal);
+
 }
+	public static void main(String[] args)
+	{
+		EmpWage ew=new EmpWage();
+		ew.empSal();
+	}
+
+
+}
+
