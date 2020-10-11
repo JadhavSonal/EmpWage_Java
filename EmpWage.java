227fdea3c89a5226b17b863c2cb3a6b107600194
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-10-11 12:03:26 +0530

    Added: Part Time Emp Wage

--- EmpWage.java
+++ EmpWage.java
@@ -3,14 +3,14 @@ import java.util.Random;
 public class EmpWage
 {
 	public static void main(String agrs[])
-	{	 
-		int PartTimeHour=4;
-		 
+	{
+
+
 		System.out.println("Welcome to Employee Wage Compuation");
 		Random rand=new Random();
 		int i=rand.nextInt(2);
 		int isPresent=1;
-		int WagePerHour=20, HourPerDay=8, ParttimeHour=4;
+		int WagePerHour=20, HourPerDay=8, PartTimeHour=4;
 
 		if(i==1)
 		{
@@ -23,7 +23,12 @@ public class EmpWage
 		System.out.println("Calculate Daily Employee Wage");
 		int WagePerDay = (WagePerHour * HourPerDay);
 		System.out.println("Daily Employee Wage:"+WagePerDay);
-	
+		
+		System.out.println("Calculate Part time Employee Wage");
+                int WagePerDay1 = (WagePerHour * PartTimeHour);
+                System.out.println("Part time Employee Wage:"+WagePerDay1);
+
+
 		
 	}
 }
